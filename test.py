0fdf35a76a5657d0b7395db4c9e04a6e1fbe5e8c
jonathankraft16 <58642819+jonathankraft16@users.noreply.github.com>
2021-04-09 15:52:38 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,56 +1,37 @@
-import RPi.GPIO as GPIO
-from lib_nrf24 import NRF24
-import time
+import RPi.GPIO as GPIO 
+import time     
 import spidev
+from lib_nrf24 import NRF24 
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM)  
 
-pipes = [[0xE8, 0xE8, 0xF0, 0xF0, 0xE1], [0xF0, 0xF0, 0xF0, 0xF0, 0xE1]]
+pipes = [[0xE0, 0xE0, 0xF1, 0xF1, 0xE0], [0xF1, 0xF1, 0xF0, 0xF0, 0xE0]]
 
-radio = NRF24(GPIO, spidev.SpiDev())
-radio.begin(0, 17)
-
-radio.setPayloadSize(32)
-radio.setChannel(0x76)
-radio.setDataRate(NRF24.BR_1MBPS)
+radio.begin(0, 17)  
+radio.setPayloadSize(32)  
+radio.setChannel(0x76) 
+radio.setDataRate(NRF24.BR_1MBPS)    
 radio.setPALevel(NRF24.PA_MIN)
 
-radio.setAutoAck(True)
-radio.enableDynamicPayloads()
-radio.enableAckPayload()
-
-radio.openWritingPipe(pipes[0])
-radio.openReadingPipe(1, pipes[1])
+radio.openWritingPipe(pipes[0])     
 radio.printDetails()
-#radio.startListening()
 
-message = list("GETSTRING")
-while len(message) < 32:
-    message.append(0)
+sendMessage = list("Hi..Arduino UNO")  
+while len(sendMessage) < 32:    
+    sendMessage.append(0)
 
 while True:
-    start = time.time()
-    radio.write(message)
-    print("Sent the message: {}".format(message))
-    radio.startListening()
-    
-    while not radio.available(0):
+    start = time.time()      
+    radio.write(sendMessage)   
+    print("Sent the message: {}".format(sendMessage))  
+send
+    radio.startListening()      
+
+while not radio.available(0):
         time.sleep(1/100)
         if time.time() - start > 2:
-            print("Timed out.")
+            print("Timed out.")  # print error message if radio disconnected or not functioning anymore
             break
 
-    receivedMessage = []
-    radio.read(receivedMessage, radio.getDynamicPayloadSize())
-    print("Received: {}".format(receivedMessage))
-
-    print("Translating our received Message into unicode characters...")
-    string = ""
-
-    for n in receivedMessage:
-        if (n >= 32 and n <= 126):
-            string += chr(n)
-    print("Our received message decodes to: {}".format(string))
-
-    radio.stopListening()
-    time.sleep(1)
+  radio.stopListening()     # close radio
+    time.sleep(3)  # give delay of 3 seconds
